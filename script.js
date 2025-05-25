e578f525ffaad685e1dfee4954343bcb735e435f
Sathish2909 <sathishshanmugam2904@gmail.com>
2025-05-25 19:59:38 +0530

    changed

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function scrollToFooter() {
 
 
 const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
+const navLinks = document.getElementById('nav-link');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
